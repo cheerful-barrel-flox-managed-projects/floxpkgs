743cfab86b6fb2877a92196f40e2512d6590fc38
Attila Gulyas <attila@floxdev.com>
2021-07-23 17:01:33 +0000

    Test implicit pkgs

--- pkgs/hello-python-another/default.nix
+++ pkgs/hello-python-another/default.nix
@@ -2,9 +2,9 @@
 , python3Packages
 }:
 
-python3Packages.buildPythonApplication rec {
+flox.python3Packages.buildPythonApplication rec {
   project = "hello-python";
-
+  rev = "another";
   # Programs and libraries used only at build-time.
   nativeBuildInputs = [ ];
 
@@ -14,7 +14,7 @@ python3Packages.buildPythonApplication rec {
   # The propagated equivalent of buildInputs. Note that python modules
   # must always be listed in this section on account of the way that the
   # python module support works in Nix.
-  propagatedBuildInputs = [ python3Packages.hello-python-library  ];
+  propagatedBuildInputs = [ ];
 
   # See https://nixos.org/manual/nixpkgs/stable/#ssec-stdenv-dependencies for
   # more information on the various ways of specifying dependencies with Nix.
